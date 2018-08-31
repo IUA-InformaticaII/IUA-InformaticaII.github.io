6bb10b1022a82d1e27e70d63c509a3d20716e06b
Ricardo Martin Marcucci <mmarcucci@ucc.edu.ar>
2018-08-31 12:15:26 -0300

    Se agrego filmina de recursividad

--- assets/js/runcpp.js
+++ assets/js/runcpp.js
@@ -1,20 +1,30 @@
-function run(el){
+function run(el) {
     var element = el;
-    var code = el.parentElement.getElementsByTagName("code")[0].innerText;
-    code = code.replace('&lt;','<');
-    code = code.replace('&gt;','>');
+    var code = el.parentElement.parentElement.getElementsByTagName("code")[0].innerText;
+    code = code.replace('&lt;', '<');
+    code = code.replace('&gt;', '>');
+    code = code.replace('&lt;', '<');
+    code = code.replace('\xA0', '');
 
     var input = "4321";
     var output = "";
     var config = {
         stdio: {
-            write: function(s) {
+            write: function (s) {
                 output += s;
             }
         }
     };
     var exitCode = JSCPP.run(code, input, config);
-    output = "Salida:\n"+ output;
-    el.parentElement.getElementsByClassName('output')[0].innerText = output;
+    //output = "Salida:\n" + output;
+    el.parentElement.parentElement.getElementsByClassName('output')[0].innerText = output;
 
+}
+
+function sol(el) {
+    var solu = el.getElementsByTagName("pre")[0].innerHTML;
+    var code = el.parentElement.parentElement.getElementsByTagName("code")[0];
+    code.innerHTML = solu;
+
+    hljs.highlightBlock(code);
 }
